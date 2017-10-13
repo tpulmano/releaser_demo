3e071dc9ff78a7c0733f437f8ddf0559a9853c5a
Tomas Pulmano <tomas@scopely.com>
2017-10-13 14:47:54 -0700

    Preparing release 0.0.2

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,4 +1,4 @@
-0.0.2 (unreleased)
+0.0.2 (2017-10-13)
 ------------------
 
 - Nothing changed yet.
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 
 from setuptools import setup, find_packages
 
-version = '0.0.2.dev0'
+version = '0.0.2'
 
 setup(
     name='releaser_demo',
